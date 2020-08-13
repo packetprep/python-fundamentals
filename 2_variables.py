bc6afe1c37ead93d979f34a77437c90e75d645a8
Krishna Teja <packetcode@gmail.com>
2020-08-13 16:00:48 +0530

    updated

--- 2_variables.py
+++ 2_variables.py
@@ -10,3 +10,34 @@ print(a)
 
 # Concatination of different type of varaibles
 print("Im a string" + str(123))
+
+## Checking the varaible types
+
+# Basic Datatypes
+a = 10
+print(a,type(a))
+
+a = 5.5
+print(a, type(a))
+
+a = 'Teja'
+print(a, type(a))
+
+a = True
+print(a, type(a))
+
+a = None
+print(a, type(a))
+
+# Sequence Types
+a = [23,25,36]
+print(a, type(a))
+
+a = (23,25,36)
+print(a, type(a))
+
+a = {23,45,36}
+print(a, type(a))
+
+a = {1:"Ramesh",2:"Suresh"}
+print(a, type(a))
--- start/2_variables.py
+++ start/2_variables.py
@@ -1,7 +1,13 @@
-# # Variables in Python
+## Variables in Python
 
 # Variable declaration
 
 # Changing variable values
 
 # Concatination of different type of varaibles
+
+## Checking the varaible types
+
+# Basic Datatypes
+
+# Sequence Types
